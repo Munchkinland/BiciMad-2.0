e3a1d4b6a970d8a47bef425089583c5d0dfdf848
Rubén Carrasco <92251234+Munchkinland@users.noreply.github.com>
2024-03-11 20:42:24 +0100

    falta escribir credenciales correctas api emt

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ import requests
 app = Flask(__name__)
 
 # URL base de la API
-base_url= "https://openapi.emtmadrid.es/v1"
-email = "ruben.c_ac@icloud.com"
-password = "Prada2024!"
+base_url = "https://openapi.emtmadrid.es/v1"
 
 def iniciar_sesion(email, password):
     url = f"{base_url}/mobilitylabs/user/login/"
@@ -18,9 +16,11 @@ def iniciar_sesion(email, password):
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
+        # Capturar y devolver el token de acceso
         token = response.json().get("data", [{}])[0].get("accessToken")
         return token, None
     else:
+        # Capturar y devolver el error de la solicitud
         return None, {
             "message": "Error en la solicitud de inicio de sesión:",
             "status_code": response.status_code,
@@ -30,25 +30,33 @@ def iniciar_sesion(email, password):
 @app.route('/test_conexion', methods=['GET'])
 def test_conexion():
     # Credenciales de inicio de sesión
-    email = "tu_email@example.com"  # Asegúrate de reemplazar esto con tus credenciales
-    password = "tu_contraseña"  # Asegúrate de reemplazar esto con tus credenciales
+    email = "tu_email@example.com"  # Reemplaza con tus credenciales reales
+    password = "tu_contraseña"  # Reemplaza con tus credenciales reales
 
-    # Iniciar sesión y obtener el token
     token, error = iniciar_sesion(email, password)
 
     if token:
-        # Aquí se podría hacer una solicitud adicional para probar el token,
-        # pero para simplificar, asumiremos que obtener un token significa que la conexión es exitosa
         return jsonify({"mensaje": "Conexión exitosa. Token de acceso: " + token})
     else:
-        mensaje_error = "Error en el inicio de sesión. Revisa las credenciales."
+        mensaje_error = "Error en el inicio de sesión. Por favor, revisa las credenciales."
         if error:
             mensaje_error += " " + error["message"]
         return jsonify({"mensaje": mensaje_error}), 400
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/eda')
+def eda():
+    return render_template('eda.html')
+
+@app.route('/proyectoml')
+def proyectoml():
+    return render_template('proyectoml.html')
+
 @app.route('/appuser')
 def appuser():
-    # Esta función servirá la página HTML modificada que incluye la funcionalidad AJAX
     return render_template('appuser.html')
 
 if __name__ == '__main__':
