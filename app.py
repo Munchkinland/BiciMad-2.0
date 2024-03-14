90d067a0c0c33d46fd419595c4a83242ced7a702
Tu Nombre <tuemail@example.com>
2024-03-14 15:09:47 +0100

    ngrok

--- app.py
+++ app.py
@@ -79,4 +79,5 @@ def appuser():
     return render_template('appuser.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=8080)
